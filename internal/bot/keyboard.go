974a8311f92ea0d1ded9558cf2d1d5f47f83151d
IllTamer <mail@illtamer.com>
2024-04-23 13:37:27 +0800

    feat: change query keyboard type

--- internal/bot/keyboard.go
+++ internal/bot/keyboard.go
@@ -6,29 +6,15 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var replyKeyboard = tgbotapi.NewReplyKeyboard(
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("/photos"),
-		tgbotapi.NewKeyboardButton("/videos"),
-	),
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("/texts"),
-		tgbotapi.NewKeyboardButton("/voices"),
-		tgbotapi.NewKeyboardButton("/docs"),
-	),
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("/help"),
-	),
-)
+var mainReplyKeyboard tgbotapi.InlineKeyboardMarkup
 
-// /photos 指令内联键盘
-// current: 0, 1, 2, 3
-func generatePhotosInlineKeyboard(tag string, current int, total int) *tgbotapi.InlineKeyboardMarkup {
+// 查詢指令内联键盘
+func generateQueryInlineKeyboard(tag string, current int, total int) *tgbotapi.InlineKeyboardMarkup {
 	markup := tgbotapi.NewInlineKeyboardMarkup(
 		tgbotapi.NewInlineKeyboardRow(
 			tgbotapi.NewInlineKeyboardButtonData("⬅️", fmt.Sprintf("%s#%d", tag, current-1)),
-			// TODO 点击捕获用户输入: 请输入你要跳转到的页面 (1-total)
-			tgbotapi.NewInlineKeyboardButtonData(fmt.Sprintf("%d⁄%d", current, total), NULL_CALLBACK),
+			tgbotapi.NewInlineKeyboardButtonData("⌘", HelpCommand),
+			tgbotapi.NewInlineKeyboardButtonData(fmt.Sprintf("%d⁄%d", current, total), fmt.Sprintf(">%s#1-%d", tag, total)),
 			tgbotapi.NewInlineKeyboardButtonData("➡️", fmt.Sprintf("%s#%d", tag, current+1)),
 		),
 	)
